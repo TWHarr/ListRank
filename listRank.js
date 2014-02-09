e48e8cc77860d31f238aeb0a6bd1f3ea3ea41c41
TWHarr <tharrison06@gmail.com>
2014-02-08 20:36:31 -0800

    added stats

--- listRank.js
+++ listRank.js
@@ -7,10 +7,14 @@ var counter = parseInt(localStorage.getItem('count'));
 var buildList = function(formField) {
     if (formField !== null) {
         var formField = formField.split(',');
-        var listCollapse = list.join("").toLowerCase().replace(/\s|[^a-z0-9]/g,"");
+        var listCollapse =[];
+        for (var i = 0; i < list.length; i++) {
+            listCollapse.push(list[i][0]);
+        };
+        listCollapse.join("").toLowerCase().replace(/\s|[^a-z0-9]/g,"");
         for (var i = 0; i < formField.length; i++) {
             if (listCollapse.indexOf(formField[i].toLowerCase().replace(/\s|[^a-z0-9]/g,"")) === -1) {
-                list.push(formField[i])
+                list.push([formField[i], 0, 0]);
             }
         }
     }
@@ -30,12 +34,15 @@ var select2 = function() {// selects the two items to be compared
 
 var choice = function() {
     select2();
-    var userInput = prompt(("Which do you like more? " + item1 +"? or " + item2 + "?"), "Type '1' or '2' here. Hit Cancel to stop ranking.");
+    var userInput = prompt(("Which do you like more? " + item1[0] +"? or " + item2[0] + "?"), "Type '1' or '2' here. Hit Cancel to stop ranking.");
     if (userInput === null) {
         $(".result").empty().append("No selection was made. Hit the button below to keep going!")
         displayList();
     } else if (userInput === "1") {
         counter++;
+        item1[1]++;
+        item2[1]++;
+        item1[2]++;
         $(".counter").empty().append(counter + " items!")
         last = $.extend(true, [], list);
         var space1 = list.indexOf(item1);
@@ -43,16 +50,19 @@ var choice = function() {
         if (space2 < space1) {
             var switched = list.splice(space1,1);
             var newList = list.splice(space2,0,item1);
-            $(".result").empty().append(item1 + " moved to position " + (list.indexOf(item1)+1) + " in your list!");
+            $(".result").empty().append(item1[0] + " moved to position " + (list.indexOf(item1)+1) + " in your list! You've ranked " + item1[0] + " " + item1[1] + " times, and it wins " + Math.round(((item1[2] * 100) / item1[1])) +"% of the time." );
             displayList();
             choice();
         } else {
-            $(".result").empty().append("Your list remains unchanged:");
+            $(".result").empty().append(item1[0] + " is already ahead of " + item2[0] + ". Your list remains unchanged. You've ranked " + item1[0] + " " + item1[1] + " times, and it wins " + Math.round(((item1[2] * 100) / item1[1])) +"% of the time.");
             displayList();
             choice();
         }
     } else if (userInput === "2") {
         counter++;
+        item1[1]++;
+        item2[1]++;
+        item2[2]++;
         $(".counter").empty().append(counter + " items!")
         last = $.extend(true, [], list);
         var space1 = list.indexOf(item1);
@@ -60,11 +70,11 @@ var choice = function() {
         if (space1 < space2) {
             var switched = list.splice(space2,1);
             var newList = list.splice(space1,0,item2);
-            $(".result").empty().append(item2 + " moved to position " + (list.indexOf(item2)+1) + " in your list!");
+            $(".result").empty().append(item2[0] + " moved to position " + (list.indexOf(item2)+1) + " in your list! You've ranked " + item2[0] + " " + item2[1] + " times, and it wins " + Math.round(((item2[2] * 100) / item2[1])) +"% of the time." );
             displayList();
             choice();
         } else {
-            $(".result").empty().append("Your list remains unchanged.");
+            $(".result").empty().append(item2[0] + " is already ahead of " + item1[0] + ". Your list remains unchanged. You've ranked " + item2[0] + " " + item2[1] + " times, and it wins " + Math.round(((item2[2] * 100) / item2[1])) +"% of the time.");
             displayList();
             choice();
         }
@@ -88,9 +98,11 @@ var displayList = function(){ //displays the updated list on the page
     localStorage.setItem('cachedList', JSON.stringify(list));
     localStorage.setItem('count', counter.toString());
     $(".listHead > li").remove();
+    $(".counter").empty().append(counter + " items!");
     for (var i = 0; i < list.length; i++) {
         $(".listHead").append("<li></li>");
-        $(".listHead > li:last").append(list[i]);
+        $(".listHead > li:last").append(list[i][0]);
+
     };
 }
 
@@ -142,7 +154,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){ //clear list
     $(".delete").on("click", function() {
-        var choice = confirm("Are you surre you want to delete your list? Once you do, you can't get it back!");
+        var choice = confirm("Are you sure you want to delete your list? Once you do, you can't get it back!");
         if (choice) {
             list = [];
             counter = 0;
@@ -150,3 +162,9 @@ $(document).ready(function(){ //clear list
         }
     })
 })
+
+/*
+
+List model: [[name, total, wins], [name, total, wins],..., [name, total, wins]]
+
+*/
